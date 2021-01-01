c31e78525f35b8aa033fe11ec215c6fe0f8673b7
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-01 23:21:58 +0000

    Add js to handle changing navbar when screen width and scroll changes occur

--- static/js/base.js
+++ static/js/base.js
@@ -1,24 +1,81 @@
+/*jshint esversion: 6 */ 
 document.addEventListener("DOMContentLoaded", function() { 
-    let lastScrollTop = 20;
+    let firstScrollTop = 20;
     let nextScrollTop = 150;
-    let navbar = document.querySelector(".navbar")
+    let navbar = document.querySelector(".navbar");
+    let pageLogo = document.querySelector('#page-logo img');
+    let navLogo = document.querySelector('.navLogo img ');
 
+    /* Handle screen width changes */
+    window.addEventListener("resize", function (){
+        let scrollTop = window.pageYoffset || document.documentElement.scrollTop;
+        let width = window.innerWidth;
+        /* if the screen width moves below 768 */
+        if (width < 768){
+            /* first check if screen is at the top */
+            if (scrollTop < firstScrollTop){
+                navbar.style.top = '0px';
+                pageLogo.style.display = 'block';
+                navLogo.style.display = 'none';
+                navbar.classList.remove('fixed-navbar');
+            }
+            /* else its scrolled down */
+            else {
+                navbar.style.top = '0px';
+                pageLogo.style.display = 'none';
+                navLogo.style.display = 'block';
+                navbar.classList.add('fixed-navbar');
+            }
+        }
+        /* if the screen width moves above 768 */
+        else {
+            /* first check is screen at the top */
+            if (scrollTop < firstScrollTop){
+                navbar.style.top ='150px';
+                pageLogo.style.display = 'block';
+                navLogo.style.display = 'none';
+                navbar.classList.remove('fixed-navbar');
+            }
+            /* else the screen is scrolled down */
+            else {
+                navbar.style.top = '0px';
+                pageLogo.style.display = 'none';
+                navLogo.style.display = 'block';
+                navbar.classList.add('fixed-navbar');
+            }
+        }
+    });
+
+    /* handle change in scroll */
     window.addEventListener("scroll", function () {
         let scrollTop = window.pageYoffset || document.documentElement.scrollTop;
-        pageLogo = document.querySelector('#page-logo');
-        navLogo = document.querySelector('.navLogo img ');
-        background = document.querySelector('nav div .collapse')
-        if (scrollTop > lastScrollTop) {
-            navbar.style.top ='10px';
+        let width = window.innerWidth;
+        /* if page has been scrolled down past firstScrollTop */
+        /* same action for wide or narrow screens */
+        if (scrollTop >= firstScrollTop) {
+            navbar.style.top = '0px';
+            /* check for nextScrollTop */
             if (scrollTop > nextScrollTop) {
                 pageLogo.style.display = 'none';
                 navLogo.style.display = 'block';
-                background.apply
+                navbar.classList.add('fixed-navbar');
             }
+        /* else - page has not been scrolled and is at the top */
         } else {
-            navbar.style.top ='150px';
-            pageLogo.style.display = 'block';
-            navLogo.style.display = 'none';
+            /* first check if we are on large screen size - we should have natural navbar*/
+            if (width >= 768) {
+                navbar.style.top ='150px';
+                pageLogo.style.display = 'block';
+                navLogo.style.display = 'none';
+                navbar.classList.remove('fixed-navbar');
+            }
+            /* else we are on small screen size - not scrolled */
+            else {
+                navbar.style.top ='0px';
+                pageLogo.style.display = 'block';
+                navLogo.style.display = 'none';
+                navbar.classList.remove('fixed-navbar');
+            }
         }
     });
 });
