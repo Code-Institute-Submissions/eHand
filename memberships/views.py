4fb2c7a4d5eedfe1beb0b61633ff05bc119662e1
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-04 22:02:36 +0000

    Add a view helper funtion to models.views
    * Code enables the returning of the current membership package
    * in the context.

--- memberships/views.py
+++ memberships/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
-from .models import Packages
+from django.shortcuts import render, get_object_or_404
+from .models import Packages, Memberships
 # Create your views here.
+
+
+def get_current_package(request):
+    """ A helper funtion that retrieves the membership package that the user
+    is currently subscribed to.
+    """
+    current = get_object_or_404(Memberships, user=request.user)
+    return str(current.membership_type)
 
 
 def select_package(request):
     """ A view that returns the membership selection page """
 
     packages = Packages.objects.all()
+    current_package = get_current_package(request)
     context = {
-        'packages': packages
+        'packages': packages,
+        'current_package': current_package
     }
     return render(request, 'memberships/select_package.html', context)
