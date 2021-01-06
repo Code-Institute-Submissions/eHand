9713c1b304d272d0fa183a3a09254c9c0f04b45b
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-06 21:07:49 +0000

    Initial setup of post section of the select_package view

--- memberships/views.py
+++ memberships/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Packages, Memberships, Subscriptions
+
 # Create your views here.
 
 
@@ -33,19 +37,36 @@ def select_package(request):
     current_package = get_current_package(request)
 
     if request.method == 'POST':
-        # Get the posted info
+        # Get the posted info - then gets it in object form
         selected_package_type = request.POST.get('package_type')
-        # Get the current users subscription
-        user_subscription = get_user_subscription(request)
-
-        # check if the users current package is same as selected
         selected_package_query = get_object_or_404(
             Packages, package_type=selected_package_type)
         if selected_package_query:
             selected_package = selected_package_query
 
-        print(f"#################### selected package ------- {selected_package}")
-        print(f"#################### user subscrip ------- {user_subscription}")
+        # Get the current users subscription
+        user_subscription = get_user_subscription(request)
+
+        # ------------------
+        # Validate
+        # ------------------
+
+        # check if current = selected - this should not be possible
+        # but need to let them know they already have this package
+        if current_package.membership_type == selected_package:
+            if user_subscription is not None:
+                messages.info(request, "This is your existing membership package. \
+                    Your next payment is due on: {}".format(
+                        'need to get this from stripe'))
+                # send user back to the page they came -  packages
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        # Now we can save the selected_package into session storage
+        # because we want to pass it to the next view
+        request.session['selected_package_type'] = selected_package.package_type
+
+        return HttpResponseRedirect(reverse('packages/payment'))
+
 
 
     context = {
