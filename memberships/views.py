c8e45fda0c2ba636471ae9f54e95f1f8cd1624c5
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-06 21:56:32 +0000

    Add a payment view and a get selected package helper function

--- memberships/views.py
+++ memberships/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Packages, Memberships, Subscriptions
 
+import stripe
+
 # Create your views here.
 
 
@@ -11,6 +13,7 @@ def get_current_package(request):
     """ A helper funtion that retrieves the membership package that the user
     is currently subscribed to.
     """
+
     current = get_object_or_404(Memberships, user=request.user)
     if current:
         return current
@@ -20,12 +23,27 @@ def get_current_package(request):
 def get_user_subscription(request):
     """ A helper funtion that retrieves the users current subscription
     """
+
     # look in subscriptions for the current user
     query_subscription = Subscriptions.objects.filter(
         user_membership=get_current_package(request))
     if query_subscription.exists():
         user_subscription = query_subscription.first()
         return user_subscription
+
+
+def get_selected_package(request):
+    """ A helper funtion that gets the selected package from session storage
+    """
+    package_type = request.session['selected_package_type']
+    # convert it to an object
+
+    selected_package_query = get_object_or_404(
+            Packages, package_type=package_type)
+    if selected_package_query:
+        return selected_package_query
+    return None
+
 
 
 def select_package(request):
@@ -47,9 +65,7 @@ def select_package(request):
         # Get the current users subscription
         user_subscription = get_user_subscription(request)
 
-        # ------------------
         # Validate
-        # ------------------
 
         # check if current = selected - this should not be possible
         # but need to let them know they already have this package
@@ -63,14 +79,22 @@ def select_package(request):
 
         # Now we can save the selected_package into session storage
         # because we want to pass it to the next view
-        request.session['selected_package_type'] = selected_package.package_type
-
+        request.session[
+            'selected_package_type'] = selected_package.package_type
         return HttpResponseRedirect(reverse('packages/payment'))
-
-
 
     context = {
         'packages': packages,
         'current_package': str(current_package.membership_type)
     }
     return render(request, 'memberships/select_package.html', context)
+
+
+def payment(request):
+    """ Get the stripe payment form for the user
+        Also handle the payment
+    """
+    # get some required variables
+    current_package = get_current_package(request)
+    selected_package = get_selected_package(request)
+    publicKey = settings.STRIPE_PUBLIC_KEY
