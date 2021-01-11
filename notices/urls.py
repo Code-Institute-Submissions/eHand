369a0aa390d3f3fc6dcf81efd6d4d68a0cf7a7d6
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-11 22:04:59 +0000

    Add a create post view, url and template

--- notices/urls.py
+++ notices/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import NoticeListView, NoticeDetailView
+from .views import (
+    NoticeListView,
+    NoticeDetailView,
+    NoticeCreateView)
+
 from . import views
 
 
 urlpatterns = [
     path('', NoticeListView.as_view(), name='notice-list'),
     path('notice/<int:pk>/', NoticeDetailView.as_view(), name='notice-detail'),
+    path('notice/create/', NoticeCreateView.as_view(), name='notice-create'),
 ]
--- notices/views.py
+++ notices/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Notice
 
 
@@ -13,3 +17,8 @@ class NoticeDetailView(DetailView):
     """ Returns as a view the notice details template """
     model = Notice
 
+
+class NoticeCreateView(CreateView):
+    """ Returns as a view the create a notice template """
+    model = Notice
+    fields = ['title', 'short_description', 'long_description', ]
