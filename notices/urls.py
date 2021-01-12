7afb92382dc7e0a11785961b890da1a15b528110
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-12 21:39:37 +0000

    Complete class based view for updating notices

--- notices/urls.py
+++ notices/urls.py
@@ -2,13 +2,23 @@ from django.urls import path
 from .views import (
     NoticeListView,
     NoticeDetailView,
-    NoticeCreateView)
+    NoticeCreateView,
+    NoticeUpdateView)
 
 from . import views
 
 
 urlpatterns = [
-    path('', NoticeListView.as_view(), name='notice-list'),
-    path('notice/<int:pk>/', NoticeDetailView.as_view(), name='notice-detail'),
-    path('notice/create/', NoticeCreateView.as_view(), name='notice-create'),
+    path('',
+         NoticeListView.as_view(),
+         name='notice-list'),
+    path('notice/<int:pk>/',
+         NoticeDetailView.as_view(),
+         name='notice-detail'),
+    path('notice/create/',
+         NoticeCreateView.as_view(),
+         name='notice-create'),
+    path('notice/<int:pk>/update/',
+         NoticeUpdateView.as_view(),
+         name='notice-update'),
 ]
