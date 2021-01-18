bd36927930663a5c2a7072a09ec17a2bab58ef09
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-18 20:54:14 +0000

    add defaults to debug in environ also setup allowed hosts

--- ehand/settings.py
+++ ehand/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import environ
 import dj_database_url
 from pathlib import Path
 
@@ -21,13 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+env = environ.Env(
+    # set debug, default value
+    DEBUG=(bool, False)
+)
+
+# reading .env file
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# If DEBUG not in env then default set above is False
 DEBUG = env('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['mr-smyth-ehand.herokuapp.com', 'localhost']
 
 
 # Application definition
