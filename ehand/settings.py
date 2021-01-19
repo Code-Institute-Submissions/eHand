205809fd1c72d5c2406435c51bfa35390d7e0e11
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-19 00:04:02 +0000

    Changed bucket name to match AWS

--- ehand/settings.py
+++ ehand/settings.py
@@ -192,14 +192,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Only use AWS in Heroku
 if 'USE_AWS' in os.environ:
 
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
-
     # --- Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'mr-smyth-ehand'
+    AWS_STORAGE_BUCKET_NAME = 'ehand'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
