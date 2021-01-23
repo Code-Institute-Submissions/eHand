48268e3bff36c2d99fdd6aff23f234257256e53d
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-23 00:42:37 +0000

    Set column reverse on comment container to keep scrolled to last message
    * Also remove labels from message window and add a placeholder
      via comments.forms

--- comments/forms.py
+++ comments/forms.py
@@ -6,4 +6,17 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body', 'notice')
+        widgets = {
+            'notice': forms.HiddenInput(),
+        }
 
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        label and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['body'].widget.attrs['autofocus'] = True
+        self.fields['body'].widget.attrs[
+            'placeholder'] = "Type your comment here.."
+        self.fields['body'].label = False
--- notices/models.py
+++ notices/models.py
@@ -48,3 +48,6 @@ class Commitments(models.Model):
     title = models.CharField(max_length=100)
     short_description = models.CharField(max_length=100)
     long_description = models.TextField()
+
+    def __str__(self):
+        return self.title
