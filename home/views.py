eef6b73f567558138565199bd4220a40efa10ef7
Eamonn Smyth <e.smyth75@gmail.com>
2021-01-31 22:00:35 +0000

    Check Debug settings

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 # Create your views here.
 def index(request):
     """ A view that returns the home page """
 
-    return render(request, 'home/index.html')
+    debug_flag = settings.DEBUG
+    context = {'debug_flag': debug_flag}
+
+    return render(request, 'home/index.html', context)
