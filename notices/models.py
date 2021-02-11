c06db9cd798a2dd6f996bb707ec5bfaa94166a8e
Eamonn Smyth <e.smyth75@gmail.com>
2021-02-11 17:15:38 +0000

    add specify_time field to notices.models

--- notices/models.py
+++ notices/models.py
@@ -31,7 +31,7 @@ class Notice(models.Model):
             TIME_OPTIONS, key=itemgetter(0)),
         verbose_name="Duration / Time Payment"
     )
-    specify_date = models.DateField(default=date.today)
+    specify_date = models.DateField(default=timezone.now)
     event_location_postcode = models.CharField(max_length=10)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='author')
